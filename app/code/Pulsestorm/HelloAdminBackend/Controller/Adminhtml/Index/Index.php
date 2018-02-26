0ebd39a8f46c747083a42b14efae69f02ae504ed
kolesnik-sashko <kolesnik.aleksandr777@gmail.com>
2018-02-26 17:26:54 +0000

    add HelloAdminBackend module

---
+++ app/code/Pulsestorm/HelloAdminBackend/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,24 @@
+<?php
+namespace Pulsestorm\HelloAdminBackend\Controller\Adminhtml\Index;
+class Index extends \Magento\Backend\App\Action
+{
+    
+    const ADMIN_RESOURCE = 'Index';       
+        
+    protected $resultPageFactory;
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;        
+        parent::__construct($context);
+    }
+    
+    public function execute()
+    {
+        $page = $this->resultPageFactory->create();
+        $page->setActiveMenu('Pulsestorm_HelloAdminBackend::a_menu_item');
+        $page->getConfig()->getTitle()->prepend(__('Our Custom Title'));
+        return $page;
+    }
+}
